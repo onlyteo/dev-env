da53b0a2a4584de2257e537a992e8a18d6da75cf
Teo <onlyteo@live.com>
2025-11-10 21:56:57 +0100

    Key mapping

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,3 +1,10 @@
 -- ### KEY MAP ###
 
-vim.keymap.set("n", "<C-wH>", ":split<CR>")
+-- Navigation
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+
+-- Split window
+vim.keymap.set("n", "<C-w+S-h>", ":split<CR>")
