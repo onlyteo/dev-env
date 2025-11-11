fa7198033e9941cf94b12dedbf903def596554df
Teo <onlyteo@live.com>
2025-11-11 21:06:51 +0100

    Key map nvim navigator

--- .config/nvim/lua/plugins/tmux-navigator.lua
+++ .config/nvim/lua/plugins/tmux-navigator.lua
@@ -5,5 +5,9 @@ return {
         vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>")
         vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>")
         vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>")
+        vim.keymap.set("n", "<C-Left>", ":TmuxNavigateLeft<CR>")
+        vim.keymap.set("n", "<C-Down>", ":TmuxNavigateDown<CR>")
+        vim.keymap.set("n", "<C-Up>", ":TmuxNavigateUp<CR>")
+        vim.keymap.set("n", "<C-Right>", ":TmuxNavigateRight<CR>")
     end
 }
