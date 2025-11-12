1dd06b607b248ac336c413ecc9dc70bbf17bcfc6
Teo <onlyteo@live.com>
2025-11-12 17:56:10 +0100

    Added telescope ui select

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,31 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+        config = function()    
+            local plugin = require("telescope.builtin")
+            
+            vim.keymap.set("n", "<leader>ff", plugin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", plugin.live_grep, {})
+        end
     },
-    config = function()    
-        local plugin = require("telescope.builtin")
-        
-        vim.keymap.set("n", "<leader>ff", plugin.find_files, {})
-        vim.keymap.set("n", "<leader>fg", plugin.live_grep, {})
-    end
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            local plugin = require("telescope")
+            local themesPlugin = require("telescope.themes")
+            plugin.setup({
+                extensions = {
+                    ["ui-select"] = {
+                        themesPlugin.get_dropdown {
+                        }
+                    }
+                }
+            })
+            plugin.load_extension("ui-select")
+        end
+    }
 }
